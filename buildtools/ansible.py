841687a2c8a1adca73d1c1290d9d508bfc2f7855
Michael Oultram <michael.oultram@pexip.com>
2023-04-14 16:34:10 +0100

    use buildx for --mount support

--- buildtools/ansible.py
+++ buildtools/ansible.py
@@ -13,28 +13,28 @@ def syntax(c):
 def debian_bookworm(c):
     """run ansible inside a container to validate the ansible config"""
     baseimage = "debian:bookworm"
-    c.run(f"docker build --build-arg baseimage={baseimage} {c.root_dir}")
+    c.run(f"docker buildx build --build-arg baseimage={baseimage} {c.root_dir}")
 
 
 @task(syntax)
 def debian_bullseye(c):
     """run ansible inside a container to validate the ansible config"""
     baseimage = "debian:bullseye"
-    c.run(f"docker build --build-arg baseimage={baseimage} {c.root_dir}")
+    c.run(f"docker buildx build --build-arg baseimage={baseimage} {c.root_dir}")
 
 
 @task(syntax)
 def ubuntu_focal(c):
     """run ansible inside a container to validate the ansible config"""
     baseimage = "ubuntu:20.04"
-    c.run(f"docker build --build-arg baseimage={baseimage} {c.root_dir}")
+    c.run(f"docker buildx build --build-arg baseimage={baseimage} {c.root_dir}")
 
 
 @task(syntax)
 def ubuntu_jammy(c):
     """run ansible inside a container to validate the ansible config"""
     baseimage = "ubuntu:22.04"
-    c.run(f"docker build --build-arg baseimage={baseimage} {c.root_dir}")
+    c.run(f"docker buildx build --build-arg baseimage={baseimage} {c.root_dir}")
 
 
 @task(ubuntu_jammy, debian_bookworm, ubuntu_focal, debian_bullseye, default=True)
